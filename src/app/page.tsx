255e3f800b4237e4c2504a6ebe9e6914396f4ac4
R. LaRoi <are@raewhoo.com>
2024-09-28 12:43:05 -0400

    tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
         <div className="inner-content text-2xl flex items-center justify-center h-screen">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 hero-text">
             <div className="flex items-center justify-start">
-              <div>creative</div>
+              <div>CREATIVE</div>
             </div>
 
             <div className="flex flex-col items-end justify-center">
-              <div className="text text-[#F2FF01]">web</div>  {/* Original font color */}
-              <div className="text pt-14">agency</div>  {/* Original font color */}
+              <div className="text text-[#F2FF01]">WEB</div>  {/* Original font color */}
+              <div className="text pt-14">AGENCY</div>  {/* Original font color */}
             </div>
           </div>
         </div>
