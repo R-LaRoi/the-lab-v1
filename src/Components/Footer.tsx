0d40e7957c0b2371726eaf24a4e861f49fd7d6ce
R. LaRoi <are@raewhoo.com>
2024-11-19 14:35:24 -0500

    Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -12,14 +12,14 @@ export default function Footer() {
     <div className='relative h-[700px] md:h-[100vh] bg-[#f1fd03] w-screen left-[50%] right-[50%] mx-[-50vw]'
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}>
       <div className='fixed bottom-0 left-0 right-0 w-full'>
-        <div className='footer m-5'>
-          <div className='bg-[#f1fd03] py-3 px-4 md:px-12 h-full w-full flex flex-col justify-between'>
+        <div className='footer md:flex md:justify-center md:items-end md:h-full'>
+          <div className='py-5 px-6 md:py-5 md:px-16 md:w-3/4 h-full w-full flex flex-col justify-between'>
             <div className="flex-grow"></div>
-            <div className="relative mb-16 text-5xl">
+            <div className="relative mb-16 text-7xl">
               <span className=''><div className="profile-picture "></div> Let's work </span><br />
               <span>together</span>
 
-              <div className="mt-4">
+              <div className="mt-8">
                 <hr className="border-t border-gray-800" />
               </div>
 
@@ -27,13 +27,13 @@ export default function Footer() {
                 <a
                   href="#"
                   className="
-        inline-flex items-center justify-center
-        w-[130px] h-[130px] rounded-full bg-gray-900
-        hover:bg-gray-800
-        text-white font-bold text-base text-center
-        transition-all duration-300 ease-in-out
-        transform hover:scale-105
-      "
+                inline-flex items-center justify-center
+                w-[130px] h-[130px] rounded-full bg-gray-900
+                hover:bg-gray-800
+                text-white font-bold text-base text-center
+                transition-all duration-300 ease-in-out
+                transform hover:scale-105
+              "
                 >
                   <span>Get in touch</span>
                 </a>
@@ -46,13 +46,13 @@ export default function Footer() {
                   <a
                     href="mailto:info@ahlab.co"
                     className="
-        block w-full p-4 rounded-full
-        border border-black hover:border-gray-800
-        text-black hover:text-gray-800 text-base
-        transition-all duration-300 ease-in-out
-        transform hover:scale-105
-        text-center truncate
-      "
+                  block w-full p-4 rounded-full
+                  border border-black hover:border-gray-800
+                  text-black hover:text-gray-800 text-base
+                  transition-all duration-300 ease-in-out
+                  transform hover:scale-105
+                  text-center truncate
+                "
                     title="info@ahlab.co"
                   >
                     <span>info@ahlab.co</span>
@@ -62,13 +62,13 @@ export default function Footer() {
                   <a
                     href="mailto:"
                     className="
-        block w-full p-4 rounded-full
-        border border-black hover:border-gray-800
-        text-black hover:text-gray-800 text-base
-        transition-all duration-300 ease-in-out
-        transform hover:scale-105
-        text-center truncate
-      "
+                  block w-full p-4 rounded-full
+                  border border-black hover:border-gray-800
+                  text-black hover:text-gray-800 text-base
+                  transition-all duration-300 ease-in-out
+                  transform hover:scale-105
+                  text-center truncate
+                "
                     title=""
                   >
                     <span>info@ahlab.co</span>
@@ -77,12 +77,11 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className='flex justify-between w-full text-xs mt-[5%] py-4'>
-            <div className='p-8'>©2024 AHC</div>
-            <div className='uppercase p-8 '>cookie policy</div>
-          </div>
         </div>
-
+        <div className='flex justify-between w-full text-xs mt-[5%] py-4 md:w-1/2 md:mx-auto'>
+          <div className='p-8'>©2024 AHC</div>
+          <div className='uppercase p-8'>cookie policy</div>
+        </div>
       </div>
     </div>
 
