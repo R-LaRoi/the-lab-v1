9289efb435426652effcb9552f10d2b03cc0f4e5
R. LaRoi <are@raewhoo.com>
2024-11-19 13:49:39 -0500

    globals.css

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,77 +9,81 @@ const translation = -76;
 
 export default function Footer() {
   return (
-    <div className='relative h-[600px] md:h-[50vh] bg-[#f1fd03] w-screen left-[50%] right-[50%] mx-[-50vw]'
+    <div className='relative h-[700px] md:h-[100vh] bg-[#f1fd03] w-screen left-[50%] right-[50%] mx-[-50vw]'
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}>
-
-      <div className="container medium">
-        <div className="row">
-          <div className="flex-col">
-            <div className="arrow" style={{ transform: 'rotate(15deg)' }}>
-              {/* <ArrowSvg /> */}
-            </div>
-            <h2>
-              <span><div className="profile-picture"></div> Let's work </span>
+      <div className='fixed bottom-0 left-0 right-0 w-full'>
+        <div className='footer m-5'>
+          <div className='bg-[#f1fd03] py-3 px-4 md:px-12 h-full w-full flex flex-col justify-between'>
+            <div className="flex-grow"></div>
+            <div className="relative mb-16 text-5xl">
+              <span className=''><div className="profile-picture "></div> Let's work </span><br />
               <span>together</span>
-            </h2>
-          </div>
-        </div>
-        <div className="row mt-[10%]">
-          <div className="relative flex-col m-4">
-            <div className="absolute w-full top-1/2 transform -translate-y-1/2 z-0">
-              <hr className="border-t border-gray-800" />
-            </div>
-            <div className="relative flex justify-end items-center z-10">
-              <a
-                href="https://dennissnellenberg.com/contact"
-                className="
+
+              <div className="mt-4">
+                <hr className="border-t border-gray-800" />
+              </div>
+
+              <div className="absolute bottom-0 right-0 transform translate-y-1/2">
+                <a
+                  href="#"
+                  className="
         inline-flex items-center justify-center
-        w-[150px] h-[150px] rounded-full bg-gray-900
+        w-[130px] h-[130px] rounded-full bg-gray-900
         hover:bg-gray-800
         text-white font-bold text-base text-center
         transition-all duration-300 ease-in-out
-        transform hover:scale-105  -translate-x-[15%]
+        transform hover:scale-105
       "
+                >
+                  <span>Get in touch</span>
+                </a>
+              </div>
+            </div>
 
-              >
-                <span className="block">Get in touch</span>
-              </a>
+            <div className="w-full flex flex-col space-y-8 md:flex-row md:justify-between md:items-end md:space-y-0 md:space-x-8">
+              <div className="flex flex-col md:flex-row md:space-x-4 mt-8">
+                <div className="mb-4 md:mb-0 flex-1 md:flex-none md:w-64">
+                  <a
+                    href="mailto:info@ahlab.co"
+                    className="
+        block w-full p-4 rounded-full
+        border border-black hover:border-gray-800
+        text-black hover:text-gray-800 text-base
+        transition-all duration-300 ease-in-out
+        transform hover:scale-105
+        text-center truncate
+      "
+                    title="info@ahlab.co"
+                  >
+                    <span>info@ahlab.co</span>
+                  </a>
+                </div>
+                <div className="mb-4 md:mb-0 flex-1 md:flex-none md:w-64">
+                  <a
+                    href="mailto:"
+                    className="
+        block w-full p-4 rounded-full
+        border border-black hover:border-gray-800
+        text-black hover:text-gray-800 text-base
+        transition-all duration-300 ease-in-out
+        transform hover:scale-105
+        text-center truncate
+      "
+                    title=""
+                  >
+                    <span>info@ahlab.co</span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        {/* button----- */}
-        <div className=" px-5 mt-[20%]">
-          <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
-            <div className="mb-4 md:mb-0 flex-1 md:flex-none md:w-64">
-              <a
-                href="mailto:info@dennissnellenberg.com"
-                className="
-          block w-full  p-4 rounded-full
-          border border-black hover:border-gray-800
-          text-black hover:text-gray-800  text-base
-          transition-all duration-300 ease-in-out
-          transform hover:scale-105
-          text-center truncate "
-                title="info@dennissnellenberg.com"
-              >
-                <span>info@ahlab.co</span>
-              </a>
-            </div>
-            <div className="flex-1 md:flex-none md:w-64 ">
-              <a href="#"
-                className="block w-full  p-4 rounded-full
-                border border-black hover:border-gray-800
-                text-black hover:text-gray-800  text-base
-                transition-all duration-300 ease-in-out
-                transform hover:scale-105 text-center" >
-                <span>+1 000 000 0000</span>
-              </a>
-            </div>
+          <div className='flex justify-between w-full text-xs mt-[5%] py-4'>
+            <div className='p-8'>©2024 AHC</div>
+            <div className='uppercase p-8 '>cookie policy</div>
           </div>
         </div>
+
       </div>
-
-
     </div>
 
   )
