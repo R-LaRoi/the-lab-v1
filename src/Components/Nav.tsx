f01320076f5182f538fdd696d07e50bc48392388
R. LaRoi <are@raewhoo.com>
2024-11-28 13:18:34 -0500

    Nav Update

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -67,10 +67,10 @@ export const NavBar = (): React.ReactElement => {
               </div>
               <div className='p-10'>
                 <ul className='nav-list text-left text-zinc-400'>
-                  <Link href='/services'><li className='li-list'><span>services</span></li></Link>
-                  <Link href='/Projects'><li className='li-list'><span>work</span></li></Link>
-                  <Link href='/Menu/connect'><li className='li-list'><span>contact</span></li></Link>
-                  <Link href='/'><li className='li-list'><span>home</span></li></Link>
+                  {/* <Link href='/services'><li className='li-list'><span>services</span></li></Link> */}
+                  <Link href='/Pages/Projects'><li className='li-list'><span>Projects</span></li></Link>
+                  {/* <Link href='/contact'><li className='li-list'><span>contact</span></li></Link>
+                  <Link href='/'><li className='li-list'><span>home</span></li></Link> */}
                 </ul>
               </div>
             </motion.div>
--- src/Pages/Projects/page.tsx
+++ src/Pages/Projects/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 type Props = {}
 
-export default function page({ }: Props) {
+export default function Projects({ }: Props) {
   return (
     <div>projects page</div>
   )
